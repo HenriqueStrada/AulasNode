e60a7b55b4a664e24106d471e042889c0ff384bb
Henrique Strada <henriquestradax@gmail.com>
2023-10-21 13:37:05 -0300

    Initial commit

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
-const server = http.createServer((req,res)=>{
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
+const server = http.createServer((req, res) => {
+    if(req.url === "/danki") {
+        fs.readFile("index.html", function (err, data) {
+            res.writeHead(200,{"Content-Type":"text/html"});
+            res.write(data);
+            return res.end();
+        });
+    } else {
+        fs.readFile("erro.html", function (err, data) {
+            res.writeHead(200,{"Content-Type":"text/html"});
+            res.write(data);
+            return res.end();
+        });
+    }
+});
 
-    res.end("Hello Wolrd!")
-})
-
-server.listen(port,hostname,()=>{
-    console.log("Servido esta rodando.")
-})
-
+server.listen(port, hostname, () => {
+    console.log("Servidor esta rodando.")
+});
